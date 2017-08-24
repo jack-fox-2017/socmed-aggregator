f39c8bca3b82d6e5cc3828ce9dbcf68e7b47a0d8
anggraito <anggraito.laras@gmail.com>
2017-08-24 18:51:18 +0700

    fitur timeline dan post

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -12,27 +12,52 @@ $(document).ready(function() {
     });
   });
 
-  $('#tweet').hide()
+  // $('#tweet').hide()
+  // $.ajax({
+  //   url: 'http://localhost:3000/twitter/timeline',
+  //   type: 'GET',
+  //   dataType: 'json',
+  //   success: function(data) {
+  //     data.forEach(e =>{
+  //       var template = $($('#tweet').prop('content')).children().clone()
+  //       console.log(e.user);
+  //       template.find('.body').text(e.text);
+  //       template.find('.name').text(e.user.name);
+  //       template.find('img').attr('src',e.user.profile_image_url);
+  //       template.find('.user').text('@' + e.user.screen_name);
+  //       template.find('.favorite').text('RT ' + e.favorite_count);
+  //       template.find('.retweet').text('Fav ' + e.retweet_count);
+  //
+  //       $('#timeline').append(template)
+  //     })
+  //   },
+  //   fail: function(err) {
+  //     console.log(err)
+  //   }
+  // })
+
+  $('#tweet-output').hide()
   $.ajax({
     url: 'http://localhost:3000/twitter/timeline',
     type: 'GET',
-    dataType: 'json',
     success: function(data) {
-      data.forEach(e =>{
+      var content = JSON.parse(data);
+
+      for(let i = 0; i < content.length; i++) {
         var template = $($('#tweet').prop('content')).children().clone()
-        console.log(e.user);
-        template.find('.body').text(e.text);
-        template.find('.name').text(e.user.name);
-        template.find('img').attr('src',e.user.profile_image_url);
-        template.find('.user').text('@' + e.user.screen_name);
-        template.find('.favorite').text('RT ' + e.favorite_count);
-        template.find('.retweet').text('Fav ' + e.retweet_count);
+        console.log(content[i].user);
+        template.find('.body').text(content[i].text);
+        template.find('.name').text(content[i].user.name);
+        template.find('img').attr('src',content[i].user.profile_image_url);
+        template.find('.user').text('@' + content[i].user.screen_name);
+        template.find('.favorite').text('RT ' + content[i].favorite_count);
+        template.find('.retweet').text('Fav ' + content[i].retweet_count);
 
         $('#timeline').append(template)
-      })
+      }
     },
-    fail: function(err) {
-      console.log(err)
+    fail: function() {
+      console.log('Error')
     }
   })
 
